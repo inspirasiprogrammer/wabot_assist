818e3ac1587aef637eae3322a9f9e14214d65dfd
Irwan Syahputra <bDa3qviHDaWS6aL>
2020-12-21 08:23:19 +0700

    Update

--- main.go
+++ main.go
@@ -26,6 +26,34 @@ func main() {
 		return
 	}
 
+	//load saved session
+	session, err := readSession()
+	if err == nil {
+		//restore session
+		session, err = wac.RestoreWithSession(session)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "restoring failed: %v\n", err)
+			return
+		}
+	} else {
+		//no saved session -> regular login
+		qr := make(chan string)
+		go func() {
+			terminal := qrcodeTerminal.New()
+			terminal.Get(<-qr).Print()
+		}()
+		session, err = wac.Login(qr)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error during login: %v\n", err)
+		}
+	}
+
+	//save session
+	err = writeSession(session)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error saving session: %v\n", err)
+	}
+
 	<-time.After(3 * time.Second)
 
 	previousMessage := "😘"
@@ -41,7 +69,7 @@ func main() {
 
 	msg := whatsapp.TextMessage{
 		Info: whatsapp.MessageInfo{
-			RemoteJid: "number@s.whatsapp.net",
+			RemoteJid: "6285371415511@s.whatsapp.net",
 		},
 		ContextInfo: ContextInfo,
 		Text:        "Message sent by github.com/Rhymen/go-whatsapp",
